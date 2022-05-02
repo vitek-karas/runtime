cc4e6122da9b02d4dd4b225d92aadb6bf73b2084
vitek-karas <10670590+vitek-karas@users.noreply.github.com>
2022-05-02 16:13:31 -0700

    Remove output validation and add warning checks

---
+++ src/coreclr/tools/aot/ILLink.Tests.Cases/Basic/BasicWarning.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Basic
+{
+    public class BasicWarning
+    {
+        [ExpectedWarning("IL2026", "RUC")]
+        public static void Main()
+        {
+            RUC();
+        }
+
+        [RequiresUnreferencedCode("RUC")]
+        static void RUC() { }
+    }
+}
