d19d773af0b6d1111983fbf221f64d622a5ab701
vitek-karas <10670590+vitek-karas@users.noreply.github.com>
2022-05-02 16:13:31 -0700

    Remove output validation and add warning checks

--- src/coreclr/tools/aot/ILLink.Tests/TestCasesRunner/TrimmedTestCaseResult.cs
+++ src/coreclr/tools/aot/ILLink.Tests/TestCasesRunner/TrimmedTestCaseResult.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using ILLink.Tests.TestCasesRunner;
 using Mono.Linker.Tests.Extensions;
 using Mono.Linker.Tests.TestCases;
 
@@ -10,21 +11,21 @@ namespace Mono.Linker.Tests.TestCasesRunner
     {
         public readonly TestCase TestCase;
         public readonly NPath InputAssemblyPath;
-        public readonly NPath OutputAssemblyPath;
         public readonly NPath ExpectationsAssemblyPath;
         public readonly TestCaseSandbox Sandbox;
         public readonly TestCaseMetadataProvider MetadataProvider;
         public readonly ManagedCompilationResult CompilationResult;
+        public readonly TestLogWriter LogWriter;
 
-        public TrimmedTestCaseResult(TestCase testCase, NPath inputAssemblyPath, NPath outputAssemblyPath, NPath expectationsAssemblyPath, TestCaseSandbox sandbox, TestCaseMetadataProvider metadataProvider, ManagedCompilationResult compilationResult)
+        public TrimmedTestCaseResult(TestCase testCase, NPath inputAssemblyPath, NPath expectationsAssemblyPath, TestCaseSandbox sandbox, TestCaseMetadataProvider metadataProvider, ManagedCompilationResult compilationResult, TestLogWriter logWriter)
         {
             TestCase = testCase;
             InputAssemblyPath = inputAssemblyPath;
-            OutputAssemblyPath = outputAssemblyPath;
             ExpectationsAssemblyPath = expectationsAssemblyPath;
             Sandbox = sandbox;
             MetadataProvider = metadataProvider;
             CompilationResult = compilationResult;
+            LogWriter = logWriter;
         }
     }
 }
--- src/coreclr/tools/aot/ILLink.Tests/TestCasesRunner/TrimmerDriver.cs
+++ src/coreclr/tools/aot/ILLink.Tests/TestCasesRunner/TrimmerDriver.cs
@@ -6,16 +6,19 @@ using System.IO;
 using System.Reflection;
 using System.Reflection.PortableExecutable;
 using System.Runtime.Loader;
+using ILCompiler;
 
 namespace Mono.Linker.Tests.TestCasesRunner
 {
     public class TrimmerDriver
     {
-        public void Trim (string rspFilePath)
+        public void Trim (string rspFilePath, ILogWriter logWriter)
         {
             Assembly ilcAssembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName("ilc"));
             Type programType = ilcAssembly.GetType("ILCompiler.Program")!;
             object programObject = Activator.CreateInstance(programType, nonPublic: true)!;
+            FieldInfo testLogWriterField = programType.GetField("_testLogWriter", BindingFlags.NonPublic | BindingFlags.Instance)!;
+            testLogWriterField.SetValue(programObject, logWriter);
             MethodInfo runMethod = programType.GetMethod("Run", BindingFlags.NonPublic | BindingFlags.Instance)!;
             runMethod.Invoke(programObject, new object[] { new string[] { "@" + rspFilePath } });
         }
