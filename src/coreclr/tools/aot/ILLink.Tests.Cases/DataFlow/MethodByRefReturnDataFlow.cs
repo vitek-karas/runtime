bbc3193503370d4534a523bc8ae93ca775666a3d
vitek-karas <10670590+vitek-karas@users.noreply.github.com>
2022-05-20 07:18:26 -0700

    Add data flow tests

---
+++ src/coreclr/tools/aot/ILLink.Tests.Cases/DataFlow/MethodByRefReturnDataFlow.cs
@@ -0,0 +1,49 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Helpers;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.DataFlow
+{
+	[SkipKeptItemsValidation]
+	[ExpectedNoWarnings]
+	public class MethodByRefReturnDataFlow
+	{
+		public static void Main ()
+		{
+			ReturnAnnotatedTypeReferenceAsUnannotated ();
+			AssignToAnnotatedTypeReference ();
+		}
+
+		[DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicMethods)]
+		static Type _annotatedField;
+
+		// This should warn, as assiging to the return ref Type will assign value to the annotated field
+		// but the annotation is not propagated
+		// https://github.com/dotnet/linker/issues/2158
+		// [ExpectedWarning("IL????")]
+		static ref Type ReturnAnnotatedTypeReferenceAsUnannotated () { return ref _annotatedField; }
+
+		[return: DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicMethods)]
+		static ref Type ReturnAnnotatedTypeReferenceAsAnnotated () { return ref _annotatedField; }
+
+		// https://github.com/dotnet/linker/issues/2158
+		// [ExpectedWarning("IL2026", "Message for --TestType.Requires--")]
+		static void AssignToAnnotatedTypeReference ()
+		{
+			ref Type typeShouldHaveAllMethods = ref ReturnAnnotatedTypeReferenceAsAnnotated ();
+			typeShouldHaveAllMethods = typeof (TestTypeWithRequires); // This should apply the annotation -> cause IL2026 due to RUC method
+			_annotatedField.GetMethods (); // Doesn't warn, but now contains typeof(TestType) - no warning here is correct
+		}
+
+		public class TestTypeWithRequires
+		{
+			[RequiresUnreferencedCode ("Message for --TestType.Requires--")]
+			public static void Requires () { }
+		}
+	}
+}
