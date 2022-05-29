c2bb0b753c8c00ecdd96557f4d34561fe657e516
Tlakollo <tlcejava@microsoft.com>
2022-05-28 18:37:34 -0700

    Add unchecked clause to silence an error while building Add ProduceBy.NativeAOT and verify only verify ProducedBy.NativeAOT in ResultChecker

--- src/coreclr/tools/aot/Mono.Linker.Tests/TestCasesRunner/ResultChecker.cs
+++ src/coreclr/tools/aot/Mono.Linker.Tests/TestCasesRunner/ResultChecker.cs
@@ -79,10 +79,10 @@ namespace Mono.Linker.Tests.TestCasesRunner
             VerifyLoggedMessages(original, linkResult.LogWriter, checkRemainingErrors);
         }
 
-        static bool IsProducedByLinker(CustomAttribute attr)
+        static bool IsProducedByNativeAOT(CustomAttribute attr)
         {
             var producedBy = attr.GetPropertyValue("ProducedBy");
-            return producedBy is null ? true : ((ProducedBy)producedBy).HasFlag(ProducedBy.Trimmer);
+            return producedBy is null ? true : ((ProducedBy)producedBy).HasFlag(ProducedBy.NativeAot);
         }
 
         static IEnumerable<ICustomAttributeProvider> GetAttributeProviders(AssemblyDefinition assembly)
@@ -109,7 +109,7 @@ namespace Mono.Linker.Tests.TestCasesRunner
             {
                 foreach (var attr in attrProvider.CustomAttributes)
                 {
-                    if (!IsProducedByLinker(attr))
+                    if (!IsProducedByNativeAOT(attr))
                         continue;
 
                     switch (attr.AttributeType.Name)
